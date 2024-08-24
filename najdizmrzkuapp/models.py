a5edf56e70dd21466acbb68514a97bd0bfcbd87d
JiHRiK <ol.jirikucera@gmail.com>
2024-08-24 16:18:47 +0200

    minor changes in initial commit

--- najdizmrzkuapp/models.py
+++ najdizmrzkuapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Store(models.Model):
+    name = models.CharField(max_length=150)
+    website = models.URLField(max_length=255, blank=True, null=True)
+    city = models.CharField(max_length=150)
+    address = models.CharField(max_length=255)
+    postal = models.CharField(max_length=6) # using Charfield to ensure no digits will be lost vith save() method
