c84c436e23261a5fd5e3dd36c48ce9ba83f295b2
JiHRiK <ol.jirikucera@gmail.com>
2024-08-24 19:45:50 +0200

    fixing bugs

--- najdizmrzkuapp/models.py
+++ najdizmrzkuapp/models.py
@@ -5,7 +5,13 @@ class Store(models.Model):
     website = models.URLField(max_length=255, blank=True, null=True)
     city = models.CharField(max_length=150)
     address = models.CharField(max_length=255)
-    postal = models.CharField(max_length=6) # using Charfield to ensure no digits will be lost vith save() method
+    postal = models.CharField(max_length=6)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)  # No default needed here
+
+    class Meta:
+        ordering = ['-updated', '-created']
 
     def __str__(self) -> str:
         return self.name
+    
